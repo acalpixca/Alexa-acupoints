4480ee102a9bdb4322ec6ef79c098c3bbb734d5e
acalpixca <acalpixca@gmail.com>
2017-06-21 18:00:12 +0200

    initial commit

---
+++ src/recipes.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "RECIPE_EN_GB" : {
+        "kidney one": "In the middle of the ball of the foot.",
+        "stomach thirty six": "In the front of the leg, between shinbone and fibula, about two chun below the lower end of the kneecap."
+        
+    },
+    "RECIPE_EN_US" : {
+        "kidney 1": "In the middle of the ball of the foot.",
+        "stomach 36": "In the front of the leg, between shinbone and fibula, about two chun below the lower end of the kneecap."
+    },
+    "RECIPE_DE_DE" : {
+        "Niere ein": "Alles geradeaus.",
+        "Magen 	sechsunddreißig": "Und keine Ahnung, ja voll."
+    }
+};
