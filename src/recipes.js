894d2a3640ae202518f75bd2593737d0cc28de82
acalpixca <acalpixca@gmail.com>
2017-06-22 18:46:24 +0200

    added more acupoints

--- src/recipes.js
+++ src/recipes.js
@@ -6,7 +6,18 @@ module.exports = {
     },
     "RECIPE_EN_US" : {
         "kidney 1": "In the middle of the ball of the foot.",
-        "stomach 36": "In the front of the leg, between shinbone and fibula, about two chun below the lower end of the kneecap."
+        "stomach 36": "In the front of the leg, between shinbone and fibula, about two chun below the lower end of the kneecap.",
+		"large intestine 4": "Between the thumb and the index finger",
+		"bladder 4": "between the second and the third lumbar vertebrae",
+		"large intestine 11": "on the outer side of the elbow",
+		"heart 7": "on the external side of the wrist",
+		"gallbladder 18": "on the 9th dorsal vertebra",
+		"bladder 21": "on the twelfth dorsal vertebra",
+		"bladder 23": "on the second lumbar vertebra",
+		"gallbladder 20": "on the top center of the head",
+		"gallbladder 21": "on the shoulder",
+		"ren mai 12": "right between the end of the sternum and the belly button",
+		"gallbladder 34": "TO BE CONTINUED"
     },
     "RECIPE_DE_DE" : {
         "Niere ein": "Alles geradeaus.",
