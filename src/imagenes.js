22c1d5298e9a916e23c32942d52ff1e210910b70
Eva Sanchez Guerrero <es0518@linde.com>
2018-01-30 17:06:49 +0100

    extracted s3 image path in variable

--- src/imagenes.js
+++ src/imagenes.js
@@ -1,196 +1,198 @@
+var imagenPath = "https://s3.amazonaws.com/alexaimageseva/acupoints-media/";
+
 module.exports = {
 	"IMAGEN_EN_GB" : {
 			"kidney 1": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
 				}
-				,								
+			,								
 			
 			"stomach 36": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}			
-			,
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			,		
 			
 			"large intestine 4": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 4": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"large intestine 11": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-			}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"heart 7": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"gallbladder 18": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 21": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 23": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"gallbladder 20": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"gallbladder 21": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 12": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"gallbladder 34": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 14": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 26": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"spleen 6": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 	},
 	
 		"IMAGEN_EN_US" : {
 			"kidney 1": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
 				}
-				,								
+				,									
 			
 			"stomach 36": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}			
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"large intestine 4": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 4": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"large intestine 11": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-			}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"heart 7": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"gallbladder 18": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 21": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 23": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"gallbladder 20": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"gallbladder 21": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 12": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"gallbladder 34": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 14": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"bladder 26": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 			,
 			
 			"spleen 6": {
-				"smallImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/smallImageTest.png",
-				"largeImageUrl": "https://s3.amazonaws.com/alexaimageseva/acupoints-media/largeImageTest.png"
-				}
+				"smallImageUrl": imagenPath + "smallImageTest.png",
+				"largeImageUrl": imagenPath + "largeImageTest.png"
+				}		
 	}
 	/* poner los otros idiomas */
 	
