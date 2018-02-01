2249a578735d30e13c455c4454f78089bbde1f3d
Eva Sanchez Guerrero <es0518@linde.com>
2018-02-01 16:00:15 +0100

    added speech assets

---
+++ src/acupoints.js
@@ -0,0 +1,238 @@
+var imagenPath = "https://s3.amazonaws.com/alexaimageseva/acupoints-media/";
+
+module.exports = {
+    "ACUPOINT_EN_GB" : {
+			"kidney 1": {
+				"speech": "In the middle of the ball of the foot.",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"stomach 36": {
+				"speech": "In the front of the leg, between shinbone and fibula, about two chun below the lower end of the kneecap.",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+				
+			"large intestine 4":  {
+				"speech": "Between the thumb and the index finger",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 4": {
+				"speech": "between the second and the third lumbar vertebrae",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"large intestine 11": {
+				"speech": "on the outer side of the elbow",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"heart 7": {
+				"speech": "on the external side of the wrist",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"gallbladder 18": {
+				"speech": "on the 9th dorsal vertebrae",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 21": {
+				"speech": "on the twelfth dorsal vertebrae",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 23": {
+				"speech": "on the second lumbar vertebrae",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"gallbladder 20": {
+				"speech": "on the top center of the head",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"gallbladder 21": {
+				"speech": "On the shoulder, directly above the nipple, at the midpoint of the line connecting the seventh cervical verebrae and the acromion.",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 12": {
+				"speech": "right between the end of the sternum and the belly button",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"gallbladder 34": {
+				"speech": "on the lower, outer side of the head of the fibula",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 14": {
+				"speech": "between the seventh cervical vertebrae and the first thoracic vertebrae",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 26": {
+				"speech": "between the nose and the upper lip",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"spleen 6": {
+				"speech": "On the medial aspect of the lower leg, three chun above the medial malleolus, on the posterior border of the medial aspect of the tibia.",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			}
+    },
+    "ACUPOINT_EN_US" : {
+			"kidney 1": {
+				"speech": "In the middle of the ball of the foot.",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"stomach 36": {
+				"speech": "In the front of the leg, between shinbone and fibula, about two chun below the lower end of the kneecap.",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+				
+			"large intestine 4":  {
+				"speech": "Between the thumb and the index finger",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 4": {
+				"speech": "between the second and the third lumbar vertebrae",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"large intestine 11": {
+				"speech": "on the outer side of the elbow",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"heart 7": {
+				"speech": "on the external side of the wrist",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"gallbladder 18": {
+				"speech": "on the 9th dorsal vertebrae",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 21": {
+				"speech": "on the twelfth dorsal vertebrae",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 23": {
+				"speech": "on the second lumbar vertebrae",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"gallbladder 20": {
+				"speech": "on the top center of the head",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"gallbladder 21": {
+				"speech": "On the shoulder, directly above the nipple, at the midpoint of the line connecting the seventh cervical verebrae and the acromion.",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 12": {
+				"speech": "right between the end of the sternum and the belly button",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"gallbladder 34": {
+				"speech": "on the lower, outer side of the head of the fibula",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 14": {
+				"speech": "between the seventh cervical vertebrae and the first thoracic vertebrae",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"bladder 26": {
+				"speech": "between the nose and the upper lip",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			},
+			"spleen 6": {
+				"speech": "On the medial aspect of the lower leg, three chun above the medial malleolus, on the posterior border of the medial aspect of the tibia.",
+				"imagen": {
+					"smallImageUrl": imagenPath + "smallImageTest.png",
+					"largeImageUrl": imagenPath + "largeImageTest.png"
+				}
+			}
+    },
+    "ACUPOINT_DE_DE" : {
+			"Niere ein": "Alles geradeaus.",
+			"Magen 	sechsunddreißig": "Und keine Ahnung, ja voll."
+    }
+};
